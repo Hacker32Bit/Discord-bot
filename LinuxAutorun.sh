f2165a895813408d991eedd3fc677586f3c7dc74
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2024-11-14 15:01:04 +0400

    Final working version for Debian

--- LinuxAutorun.sh
+++ LinuxAutorun.sh
@@ -7,7 +7,7 @@ DISCORD_LOGS_PATH=$WORK_DIR/logs
 PYTHON_PATH=$WORK_DIR/.venv/bin/python
 SCRIPT_PATH=$WORK_DIR/main.py
 GDRIVE_PATH=$HOME/mnt/gdrive/Discord-bot
-END_TIME="14:50:00"
+END_TIME="06:00:00"
 
 
 echo() {
@@ -25,7 +25,7 @@ if [[ ! $sleep_seconds -gt 0 ]]; then
 fi
 
 # For debug 10 minutes or 600 seconds
-sleep_seconds=600 # IMPORTANT! dont use small number. It will get in loop of system reboot!
+# sleep_seconds=120 # IMPORTANT! dont use small number. It will get in loop of system reboot!
 
 echo "Completed! The script will stopped after $sleep_seconds seconds!"
 
