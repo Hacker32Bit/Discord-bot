db38a54647b8f0f202c0b61ff467dd84abfa8640
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2024-11-27 12:07:04 +0400

    Added network checker for discord.py and backups in LinuxAutorun.sh

--- LinuxAutorun.sh
+++ LinuxAutorun.sh
@@ -7,12 +7,14 @@ DISCORD_LOGS_PATH=$WORK_DIR/logs
 PYTHON_PATH=$WORK_DIR/.venv/bin/python
 SCRIPT_PATH=$WORK_DIR/main.py
 GDRIVE_PATH=$HOME/mnt/gdrive/Discord-bot
-END_TIME="06:00:00"
+END_TIME="06:00:00" # Set time when system should be reboot
 
 
 echo() {
   command echo $(date '+%Y-%m-%d %H:%M:%S') "$@"
 }
+
+
 
 echo "====== Script started! =================================================="
 
@@ -30,7 +32,20 @@ fi
 echo "Completed! The script will stopped after $sleep_seconds seconds!"
 
 
-echo "========================================================================="
+
+echo "====== Before start discord.py =========================================="
+
+echo "Checking internet availability and waiting for access to network..."
+
+while ! ping -c 1 -W 1 google.com; do
+    echo "Waiting for google.com - network interface might be down..."
+    sleep 30
+done
+echo "Connected!"
+
+
+
+echo "====== Starting discord.py script ======================================="
 
 # Run python script using python venv
 echo "Creating process and running python script..."
@@ -39,8 +54,20 @@ timeout -s SIGINT $sleep_seconds $PYTHON_PATH $SCRIPT_PATH
 echo "Stopped process! Ready for creating backup..."
 
 
-echo "========================================================================="
 
+echo "====== Before start backups ============================================="
+
+echo "Checking internet availability and waiting for access to network..."
+
+while ! ping -c 1 -W 1 google.com; do
+    echo "Waiting for google.com - network interface might be down..."
+    sleep 30
+done
+echo "Connected!"
+
+
+
+echo "====== Starting backups ================================================="
 # Creating backup on gdrive (logs and db)
 
 echo "Checking gdrive availability and access..."
@@ -71,7 +98,7 @@ cp -v $WORK_DIR/database.sqlite $GDRIVE_PATH/backups/databases/"$current_timesta
 echo "Completed!"
 
 
-echo "========================================================================="
+echo "====== Script work completed! ==========================================="
 
 # Restarting system for clear memory
 
