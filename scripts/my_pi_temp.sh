e6c2f1f2dda233b498eb41d41683e29a0e6a5dfa
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2024-12-30 18:50:40 +0400

    Changed path for my_pi_temp.sh

--- scripts/my_pi_temp.sh
+++ scripts/my_pi_temp.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # Script: my-pi-temp.sh
 # Purpose: Display the ARM CPU and GPU  temperature of Raspberry Pi 2/3
 # Author: Vivek Gite <www.cyberciti.biz> under GPL v2.x+
