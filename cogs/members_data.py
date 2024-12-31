200fc1bd0d01cb150d73c53353ea37b48c15401d
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2024-12-31 17:51:42 +0400

    First version Members Data implementing (Doing nothing)

--- cogs/members_data.py
+++ cogs/members_data.py
@@ -1,27 +1,19 @@
-import io
 import os
-import re
-import time
 from typing import Final
 import discord
+from discord import app_commands
 from discord.ext import commands
-import math
 import sqlite3
 from dotenv import load_dotenv
 
 load_dotenv()
-GENERAL_TEXT_CHANNEL_ID: Final[str] = os.getenv("GENERAL_TEXT_CHANNEL_ID")
-GENERAL_VOICE_CHANNEL_ID: Final[str] = os.getenv("GENERAL_VOICE_CHANNEL_ID")
-STREAMS_VOICE_CHANNEL_ID: Final[str] = os.getenv("STREAMS_VOICE_CHANNEL_ID")
-MUSIC_VOICE_CHANNEL_ID: Final[str] = os.getenv("MUSIC_VOICE_CHANNEL_ID")
-AFK_VOICE_CHANNEL_ID: Final[str] = os.getenv("AFK_VOICE_CHANNEL_ID")
 GUILD_ID: Final[str] = os.getenv("GUILD_ID")
 
 database = sqlite3.connect("database.sqlite")
 cursor = database.cursor()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS members(user_id INTEGER, guild_id INTEGER, name TEXT, surname TEXT, 
-                birthday TEXT, info TEXT)""")
+                gender INTEGER, birthday TEXT, info TEXT, invites TEXT)""")
 
 
 class MembersData(commands.Cog):
@@ -31,6 +23,19 @@ class MembersData(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("[INFO] \"Members Data\" cog is ready!")
+
+    @app_commands.command(name="about_me", description="Tell me about you :)")
+    @app_commands.choices(gender=[
+        app_commands.Choice(name='Male', value=1),
+        app_commands.Choice(name='Female', value=0),
+    ])
+    @app_commands.describe(name="What is your name?")
+    @app_commands.describe(surname="What is your surname?")
+    @app_commands.describe(gender="Select your gender")
+    @app_commands.describe(birthday="Date of Birth. Type in format 'yyyy-mm-dd'. That was secret :)")
+    async def rank_card_setting(self, interaction: discord.Interaction, name: str = None, surname: str = None,
+                                birthday: str = None, gender: app_commands.Choice[int] = 0):
+        pass
 
 
 async def setup(bot):
