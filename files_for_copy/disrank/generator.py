ed040f9f80fe630e8e5f830fc4658382b93a642c
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2025-01-01 11:05:47 +0400

    Fixed pixel size in generator.py

--- files_for_copy/disrank/generator.py
+++ files_for_copy/disrank/generator.py
@@ -122,7 +122,7 @@ class Generator:
         blank_draw.rectangle((248, 188, length_of_bar, 202), fill=DARK)
         # blank_draw.ellipse((20, 20, 218, 218), fill=(255, 255, 255, 0), outline=DARK)
 
-        profile_pic_holder.paste(profile, (29, 29, 209, 209))
+        profile_pic_holder.paste(profile, (29, 29, 210, 210))
 
         pre = Image.composite(profile_pic_holder, card, mask)
         pre = Image.alpha_composite(pre, blank)
