adc92dca381d1d9e91acdae6b3d0ae650997a8eb
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2025-01-02 10:22:14 +0400

    Added some infos in db and reconfigured queries

--- cogs/members_data.py
+++ cogs/members_data.py
@@ -6,6 +6,7 @@ from discord import app_commands
 from discord.ext import commands
 import sqlite3
 from dotenv import load_dotenv
+from dateutil.parser import parse
 
 load_dotenv()
 GUILD_ID: Final[str] = os.getenv("GUILD_ID")
@@ -13,8 +14,14 @@ GUILD_ID: Final[str] = os.getenv("GUILD_ID")
 database = sqlite3.connect("database.sqlite")
 cursor = database.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS members(user_id INTEGER, guild_id INTEGER, name TEXT, surname TEXT, gender 
-                INTEGER, birthday TEXT, region TEXT, languages TEXT, info TEXT, invites TEXT, invited_from TEXT)""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS members (user_id INTEGER NOT NULL, guild_id INTEGER NOT NULL, name TEXT, 
+surname TEXT, gender INTEGER, birthday TEXT, region TEXT, languages TEXT, info TEXT, phone TEXT, email TEXT, 
+invites TEXT, invited_from TEXT, admin_info TEXT);""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS members_privacy (user_id INTEGER NOT NULL, guild_id INTEGER NOT NULL, 
+name INTEGER NOT NULL DEFAULT 1, surname INTEGER NOT NULL DEFAULT 1, gender INTEGER NOT NULL DEFAULT 1, 
+birthday INTEGER NOT NULL DEFAULT 0, region INTEGER NOT NULL DEFAULT 1, languages INTEGER NOT NULL DEFAULT 1, 
+info INTEGER NOT NULL DEFAULT 1, phone INTEGER NOT NULL DEFAULT 0, email INTEGER NOT NULL DEFAULT 0);""")
 
 
 class MembersData(commands.Cog):
@@ -26,7 +33,9 @@ class MembersData(commands.Cog):
         print("[INFO] \"Members Data\" cog is ready!")
 
     # Add info about Member
-    @app_commands.command(name="about_me", description="Tell me about you :)")
+    @app_commands.command(name="about_update",
+                          description="Add/Update information about you. [Private] fields are private by default for "
+                                      "first input. You can change your privacy with '/about_privacy' command")
     @app_commands.choices(gender=[
         app_commands.Choice(name='Male', value=1),
         app_commands.Choice(name='Female', value=2),
@@ -35,9 +44,15 @@ class MembersData(commands.Cog):
     @app_commands.describe(name="What is your name?")
     @app_commands.describe(surname="What is your surname?")
     @app_commands.describe(gender="Select your gender")
-    @app_commands.describe(birthday="Date of Birth. Type in format 'dd-mm-yyyy'. That was secret :)")
-    async def about_me(self, interaction: discord.Interaction, name: str = None, surname: str = None,
-                       birthday: str = None, gender: app_commands.Choice[int] = 0):
+    @app_commands.describe(birthday="[Private] Date of Birth. Type in this order: date, month, year.")
+    @app_commands.describe(region="Enter your region name")
+    @app_commands.describe(languages="Enter yours languages list")
+    @app_commands.describe(info="Enter about you small info (MAX 4000 symbols)")
+    @app_commands.describe(phone="[Private] Enter your phone")
+    @app_commands.describe(email="[Private] Enter your email")
+    async def about_update(self, interaction: discord.Interaction, name: str = None, surname: str = None,
+                           gender: app_commands.Choice[int] = 0, birthday: str = None, region: str = None,
+                           languages: str = None, info: str = None, phone: str = None, email: str = None):
 
         err_messages: str = ""
 
@@ -50,7 +65,7 @@ class MembersData(commands.Cog):
         date = ""
         if birthday:
             try:
-                date = datetime.strptime(birthday, '%d-%m-%Y').date()
+                date = parse(birthday, fuzzy=False)
             except ValueError as err:
                 err_messages += str(err)
 
@@ -70,18 +85,23 @@ class MembersData(commands.Cog):
             await interaction.response.send_message(err_messages)  # NOQA
         else:
 
-            cursor.execute(f"SELECT * FROM members WHERE user_id = {interaction.user.id} AND "
-                           f"guild_id = {interaction.guild.id}")
+            cursor.execute(f"SELECT user_id, guild_id, name, surname, gender, birthday, region, languages, info, "
+                           f"phone, email FROM members "
+                           f"WHERE user_id = {interaction.user.id} AND guild_id = {interaction.guild.id}")
             result = cursor.fetchone()
 
             (user_id, guild_id, old_name, old_surname, old_gender, old_birthday, old_region, old_languages, old_info,
-             invites, invited_from) = result
+             old_phone, old_email) = result
 
             print(type(result), result)
             print(user_id, guild_id, old_name, old_surname, old_gender, old_birthday, old_region, old_languages,
-                  old_info, invites, invited_from)
+                  old_info, old_phone, old_email)
 
             if result is None:
+                cursor.execute(f"INSERT INTO members_privacy(user_id, guild_id) "
+                               f"VALUES({interaction.user.id}, {interaction.guild.id})")
+                database.commit()
+
                 cursor.execute(f"INSERT INTO members(user_id, guild_id, {exist_keys[:-2]}) "
                                f"VALUES({interaction.user.id}, {interaction.guild.id}, {keys_values[:-2]})")
                 database.commit()
