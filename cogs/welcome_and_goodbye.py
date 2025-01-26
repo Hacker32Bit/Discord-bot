f516433d9aed03bf9ff3e93059527ca4134fc7ee
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2025-01-26 21:28:36 +0400

    Change from push to append

--- cogs/welcome_and_goodbye.py
+++ cogs/welcome_and_goodbye.py
@@ -53,7 +53,7 @@ class WelcomeAndGoodbye(commands.Cog):
         print(type(invite))
         print(invite)
         print(self.invites)
-        self.invites[invite.guild.id].push(invite)
+        self.invites[invite.guild.id].append(invite)
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member) -> None:
