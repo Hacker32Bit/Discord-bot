f2d6bfd8b0ce187b8c8f5c6a188e694bc6b78e22
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2025-01-26 17:38:54 +0400

    Added refferal logging

--- cogs/member_update.py
+++ cogs/member_update.py
@@ -14,75 +14,10 @@ ADMIN_LOG_CHANNEL_ID: Final[str] = os.getenv("ADMIN_LOG_CHANNEL_ID")
 class MemberUpdate(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.invites = {}
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("[INFO] \"Member update\" cog is ready!")
-        for guild in self.client.guilds:
-            # Adding each guild's invites to our dict
-            self.invites[guild.id] = await guild.invites()
-        print("setup_hook", self.invites)
-
-    @staticmethod
-    def find_invite_by_code(invite_list, code):
-        # Simply looping through each invite in an
-        # invite list which we will get using guild.invites()
-        for inv in invite_list:
-            # Check if the invite code in this element
-            # of the list is the one we're looking for
-            if inv.code == code:
-                # If it is, we return it.
-                return inv
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-
-        # Getting the invites before the user joining
-        # from our cache for this specific guild
-
-        print("OnMemberJoin", member.guild.id)
-        invites_before_join = self.invites[member.guild.id]
-
-        # Getting the invites after the user joining
-        # so we can compare it with the first one, and
-        # see which invite uses number increased
-
-        invites_after_join = await member.guild.invites()
-
-        # Loops for each invite we have for the guild
-        # the user joined.
-
-        for invite in invites_before_join:
-
-            # Now, we're using the function we created just
-            # before to check which invite count is bigger
-            # than it was before the user joined.
-
-            if invite.uses < self.find_invite_by_code(invites_after_join, invite.code).uses:
-                # Now that we found which link was used,
-                # we will print a couple things in our console:
-                # the name, invite code used the the person
-                # who created the invite code, or the inviter.
-
-                print(f"Member {member.name} Joined")
-                print(f"Invite Code: {invite.code}")
-                print(f"Inviter: {invite.inviter}")
-
-                # We will now update our cache so it's ready
-                # for the next user that joins the guild
-
-                self.invites[member.guild.id] = invites_after_join
-
-                # We return here since we already found which
-                # one was used and there is no point in
-                # looping when we already got what we wanted
-
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        # Updates the cache when a user leaves to make sure
-        # everything is up to date
-        self.invites[member.guild.id] = await member.guild.invites()
 
     # Called when member update
     @commands.Cog.listener()
--- cogs/welcome_and_goodbye.py
+++ cogs/welcome_and_goodbye.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 import discord
 from discord.ext import commands
 import datetime
@@ -10,26 +12,124 @@ load_dotenv()
 LOG_CHANNEL_ID: Final[str] = os.getenv("LOG_CHANNEL_ID")
 
 
+database = sqlite3.connect("database.sqlite")
+cursor = database.cursor()
+
+cursor.execute("""CREATE TABLE "invites" (
+	"user_id"	INTEGER NOT NULL UNIQUE,
+	"invited_by"	INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY("user_id")
+);""")
+
+
 class WelcomeAndGoodbye(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.invites = {}
+
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("[INFO] \"Welcome & Goodbye\" cog is ready!")
+        for guild in self.client.guilds:
+            # Adding each guild's invites to our dict
+            self.invites[guild.id] = await guild.invites()
+        print("on_ready", self.invites)
+
+    @staticmethod
+    def find_invite_by_code(invite_list, code):
+        # Simply looping through each invite in an
+        # invite list which we will get using guild.invites()
+        for inv in invite_list:
+            # Check if the invite code in this element
+            # of the list is the one we're looking for
+            if inv.code == code:
+                # If it is, we return it.
+                return inv
 
     @commands.Cog.listener()
     async def on_member_join(self, member) -> None:
+
+        is_invited = False
+        inviter = None
+
+        # Getting the invites before the user joining
+        # from our cache for this specific guild
+
+        print("OnMemberJoin", member.guild.id)
+        invites_before_join = self.invites[member.guild.id]
+
+        # Getting the invites after the user joining
+        # so we can compare it with the first one, and
+        # see which invite uses number increased
+
+        invites_after_join = await member.guild.invites()
+
+        # Loops for each invite we have for the guild
+        # the user joined.
+
+        for invite in invites_before_join:
+
+            # Now, we're using the function we created just
+            # before to check which invite count is bigger
+            # than it was before the user joined.
+
+            if invite.uses < self.find_invite_by_code(invites_after_join, invite.code).uses:
+                # Now that we found which link was used,
+                # we will print a couple things in our console:
+                # the name, invite code used the the person
+                # who created the invite code, or the inviter.
+
+                print(f"Member {member.name} Joined")
+                print(f"Invite Code: {invite.code}")
+                print(f"Inviter: {invite.inviter}")
+
+                # We will now update our cache so it's ready
+                # for the next user that joins the guild
+
+                self.invites[member.guild.id] = invites_after_join
+
+                # We return here since we already found which
+                # one was used and there is no point in
+                # looping when we already got what we wanted
+
+                cursor.execute(f"SELECT user_id, invited_by WHERE user_id = "
+                       f"{member.user.id}")
+                result = cursor.fetchone()
+
+                if not result:
+                    is_invited = True
+                    inviter = invite.inviter.mention
+                    invited_by = inviter.id
+                    cursor.execute(f"INSERT INTO invites(user_id, invited_by) "
+                                   f"VALUES({inviter}, {invited_by});")
+                    database.commit()
+
+
+        # Send message in LOG_CHANNEL if not invited
         channel = await self.client.fetch_channel(LOG_CHANNEL_ID)
-        embed = discord.Embed(
-            description=f":wave: Welcome to server **{member.mention}**!",
-            color=0x4caf50,
-            timestamp=datetime.datetime.now()
-        )
+
+        if is_invited:
+            embed = discord.Embed(
+                description=f":wave: Welcome to server **{member.mention}**!\nReferred by **{inviter}**!",
+                color=0x9b59b6,
+                timestamp=datetime.datetime.now()
+            )
+        else:
+            embed = discord.Embed(
+                description=f":wave: Welcome to server **{member.mention}**!",
+                color=0x4caf50,
+                timestamp=datetime.datetime.now()
+            )
         await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member) -> None:
+        # Updates the cache when a user leaves to make sure
+        # everything is up to date
+        self.invites[member.guild.id] = await member.guild.invites()
+
+        # Send message in LOG_CHANNEL
         channel = await self.client.fetch_channel(LOG_CHANNEL_ID)
         embed = discord.Embed(
             description=f"<:hmm2:1240238779181174825> **{member.mention}** leaved from this server!",
