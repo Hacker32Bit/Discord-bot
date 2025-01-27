414a84a390a0a0d131dd3f1724eb56eba7c368c7
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2025-01-27 12:31:33 +0400

    Changed to file

--- cogs/admin_commands.py
+++ cogs/admin_commands.py
@@ -73,7 +73,7 @@ class AdminCommands(commands.Cog):
         channel = await self.client.fetch_channel(channel_id)
         print("channel_id: ", channel_id)
         print("file: ", file)
-        await channel.send(file=file)
+        await channel.send(file=await file.to_file())
 
     # Command for send message with file from Bot
     @commands.command()
@@ -84,61 +84,61 @@ class AdminCommands(commands.Cog):
         print("channel_id: ", channel_id)
         print("message: ", message)
         print("file: ", file)
-        await channel.send(message=message, file=file)
+        await channel.send(message=message, file= await file.to_file())
 
 
-# Command for add manually join user log in log channel
-@commands.command()
-@commands.has_any_role("Owner", "Admin")
-async def join_user(self, member_id, user_id) -> None:
-    channel = await self.client.fetch_channel(LOG_CHANNEL_ID)
-    embed = discord.Embed(
-        description=f":wave: Welcome to server **<@{user_id}>**!",
-        color=0x4caf50,
-        timestamp=datetime.datetime.now()
-    )
-    await channel.send(embed=embed)
+    # Command for add manually join user log in log channel
+    @commands.command()
+    @commands.has_any_role("Owner", "Admin")
+    async def join_user(self, member_id, user_id) -> None:
+        channel = await self.client.fetch_channel(LOG_CHANNEL_ID)
+        embed = discord.Embed(
+            description=f":wave: Welcome to server **<@{user_id}>**!",
+            color=0x4caf50,
+            timestamp=datetime.datetime.now()
+        )
+        await channel.send(embed=embed)
 
 
-# Command for get members dict() who reacted on post by message_id
-@commands.command()
-@commands.has_any_role("Owner", "Admin")
-async def get_members_list(self, ctx, message_id):
-    await ctx.message.delete()
-    channel = self.client.get_channel(ctx.channel.id)
-    message = await channel.fetch_message(message_id)
-    users = set()
-    for reaction in message.reactions:
-        async for user in reaction.users():
-            users.add(user)
-    await ctx.send(f"Users: {', '.join(user.mention for user in users)}")
-    return users
+    # Command for get members dict() who reacted on post by message_id
+    @commands.command()
+    @commands.has_any_role("Owner", "Admin")
+    async def get_members_list(self, ctx, message_id):
+        await ctx.message.delete()
+        channel = self.client.get_channel(ctx.channel.id)
+        message = await channel.fetch_message(message_id)
+        users = set()
+        for reaction in message.reactions:
+            async for user in reaction.users():
+                users.add(user)
+        await ctx.send(f"Users: {', '.join(user.mention for user in users)}")
+        return users
 
 
-# Command for get random winner from message reactions
-@commands.command()
-@commands.has_any_role("Owner", "Admin")
-async def giveaway_random(self, ctx, message_id, count, repeat: str = None) -> None:
-    await ctx.send(f"Participants of the competition.")
-    get_all_reacted_users = await self.get_members_list(ctx, message_id)
+    # Command for get random winner from message reactions
+    @commands.command()
+    @commands.has_any_role("Owner", "Admin")
+    async def giveaway_random(self, ctx, message_id, count, repeat: str = None) -> None:
+        await ctx.send(f"Participants of the competition.")
+        get_all_reacted_users = await self.get_members_list(ctx, message_id)
 
-    winners = " "
-    for i in range(int(count)):
-        print("inside for ", i)
-        time.sleep(random.randint(500, 1500) / 1000)
-        winner = random.choice(list(get_all_reacted_users))
+        winners = " "
+        for i in range(int(count)):
+            print("inside for ", i)
+            time.sleep(random.randint(500, 1500) / 1000)
+            winner = random.choice(list(get_all_reacted_users))
 
-        if repeat is None:
-            get_all_reacted_users.remove(winner)
+            if repeat is None:
+                get_all_reacted_users.remove(winner)
 
-        winners += f"{winner.mention},"
+            winners += f"{winner.mention},"
 
-    embed = discord.Embed(
-        description=f":tada: Congrats{winners[:len(winners) - 1]}!\nYou are very lucky in this giveaway!",
-        color=0x9C27B0,
-        timestamp=datetime.datetime.now()
-    )
-    await ctx.send(embed=embed)
+        embed = discord.Embed(
+            description=f":tada: Congrats{winners[:len(winners) - 1]}!\nYou are very lucky in this giveaway!",
+            color=0x9C27B0,
+            timestamp=datetime.datetime.now()
+        )
+        await ctx.send(embed=embed)
 
 
 async def setup(client):
