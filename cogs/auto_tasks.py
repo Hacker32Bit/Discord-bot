ab7321afcb39f89e447bdee134585c4090f0bd98
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2025-01-27 14:14:20 +0400

    Changed color

--- cogs/auto_tasks.py
+++ cogs/auto_tasks.py
@@ -20,6 +20,7 @@ ACTIVITY_GIVEAWAY_MESSAGE_ID: Final[str] = os.getenv("ACTIVITY_GIVEAWAY_MESSAGE_
 database = sqlite3.connect("database.sqlite")
 cursor = database.cursor()
 
+
 class AutoTask(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -45,21 +46,21 @@ class AutoTask(commands.Cog):
         cursor.execute(descending)
         result = cursor.fetchall()
 
-        with Image.new(mode='RGBA', size=(800, 40), color=(96, 125, 139, 255 / 70)) as image:
-            notosans_bold = os.path.join(os.path.dirname(__file__), 'assets', 'NotoSans-Bold.ttf') # NOQA
-            notosans_regular = os.path.join(os.path.dirname(__file__), 'assets', 'NotoSans-Regular.ttf') # NOQA
-            rockybilly = os.path.join(os.path.dirname(__file__), 'assets', 'Rockybilly.ttf') # NOQA
+        with Image.new(mode='RGBA', size=(800, 40), color=(144, 164, 174, 191)) as image:
+            notosans_bold = os.path.join(os.path.dirname(__file__), 'assets', 'NotoSans-Bold.ttf')  # NOQA
+            notosans_regular = os.path.join(os.path.dirname(__file__), 'assets', 'NotoSans-Regular.ttf')  # NOQA
+            rockybilly = os.path.join(os.path.dirname(__file__), 'assets', 'Rockybilly.ttf')  # NOQA
 
             # ======== Fonts to use =============
             font_normal = truetype(notosans_bold, 36, encoding='UTF-8')
             font_small = truetype(notosans_regular, 20, encoding='UTF-8')
             font_signa = truetype(rockybilly, 25, encoding='UTF-8')
 
-            WHITE = (189, 195, 199)
+            white_color = (189, 195, 199)
 
             draw = Draw(image)
 
-            draw.text((10, 10), "Test", WHITE, font=font_small)
+            draw.text((10, 10), "Test", white_color, font=font_small)
 
             for user in result:
                 print(user)
