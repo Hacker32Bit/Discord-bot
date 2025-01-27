a3eebf2e5fe6d15f559ea8b31b0e4ebac6d82668
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2025-01-27 17:53:56 +0400

    Changed to 1 hours update

--- cogs/auto_tasks.py
+++ cogs/auto_tasks.py
@@ -137,7 +137,7 @@ class AutoTask(commands.Cog):
             image = image.crop((0, 0, width, new_height))
             return image
 
-    @tasks.loop(minutes=1)
+    @tasks.loop(hours=1)
     async def update_activity_giveaways_tables(self):
         channel = await self.bot.fetch_channel(ACTIVITY_GIVEAWAY_CHANNEL_ID)
         try:
