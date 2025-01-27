fbc3f64bff53969a0fdcf5064646bd0495e56b26
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2025-01-27 11:29:36 +0400

    Fixed autotask

--- cogs/auto_tasks.py
+++ cogs/auto_tasks.py
@@ -11,20 +11,22 @@ class AutoTask(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.my_task.start()
+        self.update_activity_giveaways_tables.start()
+
+    def cog_unload(self):
+        self.my_task.cancel()
+        self.update_activity_giveaways_tables.cancel()
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("[INFO] \"AutoTask\" cog is ready!")
-
-    def cog_unload(self):
-        self.my_task.cancel()
 
     @tasks.loop(time=time)
     async def my_task(self):
         pass
         # print("Need close server!")
 
-    @tasks.loop(minutes=5)
+    @tasks.loop(minutes=1)
     async def update_activity_giveaways_tables(self):
         print("UPDATE!!!")
 
