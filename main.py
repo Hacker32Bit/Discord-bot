62774feff00df6daf718c3a7bc7dfab246c595ba
Gektor <gektor.gevorgyan@ya.ru>
2025-05-11 09:43:25 +0400

    New main.py version

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import asyncio
+import os
 import sys
+import logging
+from time import strftime
+from typing import Final
 
 import discord
 from discord.ext import commands
-from typing import Final
 from dotenv import load_dotenv
-import os
-import logging
-from time import strftime
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
 APPLICATION_ID: Final[str] = os.getenv("APPLICATION_ID")
 
 # Bot setup
-intents: discord.Intents = discord.Intents.default()
-intents.message_content = True  # NOQA
-intents.members = True  # NOQA
-intents.voice_states = True  # NOQA
-intents.guilds = True  # NOQA
-intents.presences = True  # NOQA
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.voice_states = True
+intents.guilds = True
+intents.presences = True
 
 client = commands.Bot(command_prefix='!', intents=intents, application_id=APPLICATION_ID)
 
@@ -37,37 +37,30 @@ async def load_extensions():
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client.user}')
 
 async def main():
-    try:
-        await load_extensions()
-        handler = logging.FileHandler(
-            filename=os.path.join(os.sep, "tmp", "logs", f"{strftime('%Y-%m-%d %H:%M:%S')}.log"),
-            encoding='utf-8',
-            mode="a")
-        discord.utils.setup_logging(level=logging.DEBUG, root=False, handler=handler)
-        await client.start(TOKEN)
+    await load_extensions()
 
+    log_path = os.path.join(os.sep, "tmp", "logs", f"{strftime('%Y-%m-%d %H:%M:%S')}.log")
+    handler = logging.FileHandler(filename=log_path, encoding='utf-8', mode="a")
+    discord.utils.setup_logging(level=logging.DEBUG, root=False, handler=handler)
+
+    try:
+        await client.start(TOKEN)
     except discord.HTTPException as e:
         if e.status == 429:
             print("The Discord servers denied the connection for making too many requests")
         else:
-            raise e
+            raise
+    except asyncio.CancelledError:
+        print("Bot shutdown was cancelled.")
+    finally:
+        if not client.is_closed():
+            await client.close()
 
 if __name__ == "__main__":
-    # asyncio.run(main())
     try:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+        asyncio.run(main())
     except KeyboardInterrupt:
-        print("Received Ctrl+C. Stopping gracefully...")
-        # Cancel all running tasks
-        for task in asyncio.Task.all_tasks():
-            task.cancel()
-        # Optionally: Close any open resources (sockets, files, etc.)
-        # Cleanup code here
-    finally:
-        loop.run_until_complete(client.close())  # Ensure discord aiohttp session is closed
-        loop.run_until_complete(loop.shutdown_asyncgens())  # Shutdown generators
-        loop.close()
+        print("Received Ctrl+C. Exiting cleanly...")
