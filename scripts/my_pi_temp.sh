e1c5a30deba5ad22f3a806b8119f668ba0107a50
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2026-01-01 19:16:26 +0400

    Added FAN SPEED debug

--- scripts/my_pi_temp.sh
+++ scripts/my_pi_temp.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/bash
-# Script: my-pi-temp.sh
-# Purpose: Display the ARM CPU and GPU  temperature of Raspberry Pi 2/3
-# Author: Vivek Gite <www.cyberciti.biz> under GPL v2.x+
+# Script: my_pi_temp.sh
+# Purpose: Display the ARM CPU, GPU temperature and FAN speed
 # -------------------------------------------------------
+
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
+fan=$(</sys/devices/platform/cooling_fan/hwmon/hwmon2/fan1_input)
 
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
-## ******************************************* ##
-## NOTE : ADJUST " /opt/vc/bin/vcgencmd " path ##
-## ******************************************* ##
-echo "GPU => $(/usr/bin/vcgencmd measure_temp | grep  -o -E '[[:digit:]].*')"
+
+# GPU temperature
+echo "GPU => $(/usr/bin/vcgencmd measure_temp | grep -o -E '[[:digit:]].*')"
+
+# CPU temperature
 com=$(echo "scale=1; ${cpu}/1000")
-echo "CPU => $(bc <<< $com)'C"
+echo "CPU => ${com}°C"
+
+# FAN speed
+echo "FAN SPEED => ${fan} RPM"
