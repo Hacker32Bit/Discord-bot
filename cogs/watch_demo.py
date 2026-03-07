4ffaecdc8e8680e1eb77fa8c6854811115a0d690
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2026-03-07 19:02:25 +0400

    Added next line by zero-width space

--- cogs/watch_demo.py
+++ cogs/watch_demo.py
@@ -139,7 +139,7 @@ class ProfileToggleView(discord.ui.View):
             if interaction:
                 await interaction.response.send_message(
                     "📤 Done!\n\n"
-                    f"{final_text}\n\n",
+                    f"{final_text}\n\u200b",
                     ephemeral=True,
                     view=WatchDemoView(tinyurl_data["data"]["tiny_url"], self.download_url)
                 )
