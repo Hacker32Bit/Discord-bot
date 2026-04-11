42fabb93f7d3e5fc0938788a8fe497616967b493
Gektor Gevorgyan <gektor.gevorgyan@ya.ru>
2026-04-11 14:45:11 +0400

    Changed check mark emoji

--- cogs/watch_demo.py
+++ cogs/watch_demo.py
@@ -140,7 +140,7 @@ class ProfileToggleView(discord.ui.View):
 
             if interaction:
                 await interaction.response.send_message(
-                    "📤 Done!\n\n"
+                    "✔️ Done!\n\n"
                     f"{final_text}\u200b",
                     ephemeral=True,
                     view=WatchDemoView(tinyurl_data["data"]["tiny_url"], self.download_url)
